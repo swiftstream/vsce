f70f354108ec586d0363c61c299483331a786f38
Mikhail Isaev <isaev.mihael@gmail.com>
2024-08-25 12:55:47 +0400

    🪚 Add project files

---
+++ src/helpers/selectFolderHelper.ts
@@ -0,0 +1,15 @@
+import { OpenDialogOptions, window } from "vscode"
+
+export async function selectFolder(title: string, button: string) {
+	const options: OpenDialogOptions = {
+		canSelectFolders: true,
+		canSelectFiles: false,
+		openLabel: button,
+		title: title
+	}
+	const folderUris = await window.showOpenDialog(options)
+	if (folderUris && folderUris.length > 0){
+		return folderUris[0]
+	}
+	return undefined
+}
