5f5d599d1d60769c8beb45ec79077067a022e8e2
Mikhail Isaev <isaev.mihael@gmail.com>
2024-10-24 22:21:35 +0400

    🪚 FilesHelper: add `humanFileSize` method

--- src/helpers/filesHelper.ts
+++ src/helpers/filesHelper.ts
@@ -132,3 +132,21 @@ export function findFilesRecursively(extensions: string[], folder: string, lastM
     }
     return items
 }
+
+/// Credits to https://stackoverflow.com/a/14919494/1001057
+export function humanFileSize(bytes: number, si: boolean = false, dp: number = 1) {
+    const thresh = si ? 1000 : 1024
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B'
+    }
+    const units = si 
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+    let u = -1
+    const r = 10**dp
+    do {
+        bytes /= thresh
+        ++u
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1)
+    return bytes.toFixed(dp) + ' ' + units[u]
+}
