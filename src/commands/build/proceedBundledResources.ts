3673c7166e1d9a75e790bb7e53d634d5ba8a1c43
Mikhail Isaev <isaev.mihael@gmail.com>
2025-01-18 04:00:03 +0400

    🪚 ProceedBundleResources: copy files one by one

--- src/commands/build/proceedBundledResources.ts
+++ src/commands/build/proceedBundledResources.ts
@@ -15,24 +15,51 @@ export function proceedBundledResources(options: { release: boolean }) {
     for (let f = 0; f < resourceFolders.length; f++) {
         const folder = resourceFolders[f]
         const dirPath = `${buildFolder}/${folder}`
-        const items = fs.readdirSync(dirPath)
-        for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
-            const item = items[itemIndex]
-            const fromFile = `${dirPath}/${item}`
-            // skip .map files for production
-            if (options.release && fromFile.endsWith('.map'))
-                continue
-            const isFolder = fs.statSync(fromFile).isDirectory()
-            const toFile = `${destPath}/${item}`
-            print(`📑 Copy ${isFolder ? 'folder' : 'file'} ${folder.replace('.resources', '')}/${item} → ${options.release ? buildProdFolder : buildDevFolder}/${item}`, LogLevel.Verbose)
-            if (fs.existsSync(toFile))
-                print(`🚨 \`/${item}\` ${isFolder ? 'folder' : 'file'} has been overwritten`, LogLevel.Detailed)
-            fs.cpSync(fromFile, toFile, { recursive: true, force: true })
-            try {
-                if (isFolder) fs.rmSync(fromFile, { recursive: true, force: true })
-                else fs.rmSync(fromFile)
-            } catch {}
+        // NOTE: Rewritten from cpFileSync because of EACCESS issue with folders, cause it is copying folder permissions as well
+        function proceedFolder(dirPath: string, toFolder?: string | undefined) {
+            const newDestPath = toFolder ? `${destPath}/${toFolder}` : `${destPath}`
+            if (!fs.existsSync(newDestPath))
+                fs.mkdirSync(newDestPath, { recursive: true })
+            const items = fs.readdirSync(dirPath)
+            for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+                const item = items[itemIndex]
+                const fromFile = `${dirPath}/${item}`
+                const isFolder = fs.statSync(fromFile).isDirectory()
+                if (isFolder) {
+                    proceedFolder(fromFile, item)
+                } else {
+                    // skip .map files for production
+                    if (options.release && fromFile.endsWith('.map'))
+                        continue
+                    const toFile = `${newDestPath}/${item}`
+                    print(`📑 Copy file ${folder.replace('.resources', '')}/${item} → ${options.release ? buildProdFolder : buildDevFolder}/${item}`, LogLevel.Verbose)
+                    if (fs.existsSync(toFile))
+                        print(`🚨 \`/${item}\` file has been overwritten`, LogLevel.Detailed)
+                    let data: string | undefined
+                    try {
+                        data = fs.readFileSync(fromFile, 'utf8')
+                    } catch (error) {
+                        print(`🚨 fs.openSync(${fromFile}) failed: ${error}`, LogLevel.Detailed)
+                    }
+                    if (data) {
+                        try {
+                            fs.writeFileSync(toFile, data)
+                        } catch (error) {
+                            print(`🚨 fs.writeFileSync(${toFile}) failed: ${error}`, LogLevel.Detailed)
+                        }
+                        try {
+                            fs.rmSync(fromFile)
+                            print(`🧹 Cleaned up file ${folder.replace('.resources', '')}/${item}`, LogLevel.Verbose)
+                        } catch (error) {
+                            print(`🚨 fs.rmSync(${fromFile}) failed: ${error}`, LogLevel.Detailed)
+                        }
+                    } else {
+                        print(`🚨 Skipping ${toFile}: something is wrong with it`, LogLevel.Detailed)
+                    }
+                }
+            }
         }
+        proceedFolder(dirPath)
     }
     measure.finish()
     print(`📄 Processed bundle resources in ${measure.time}ms`, LogLevel.Detailed)
