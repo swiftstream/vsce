53a1391dfd1c0b2d119abae912e7741e92625f45
Mikhail Isaev <isaev.mihael@gmail.com>
2025-01-22 04:29:06 +0400

    🪚 Implement `openDocumentInEditorOnLine`

--- src/helpers/openDocumentInEditor.ts
+++ src/helpers/openDocumentInEditor.ts
@@ -1,4 +1,4 @@
-import { commands, Uri, window, workspace } from "vscode"
+import { commands, Uri, Position, Selection, window, workspace } from 'vscode'
 
 export async function openDocumentInEditor(path: string, cursorWhereLineContains?: string) {
     const doc = await workspace.openTextDocument(Uri.parse(path))
@@ -15,4 +15,20 @@ export async function openDocumentInEditor(path: string, cursorWhereLineContains
     await commands.executeCommand('cursorMove', {
         to: 'down', by: 'wrappedLine', value: appPortLine
     })
+}
+
+export async function openDocumentInEditorOnLine(path: string, line: number, position?: number) {
+    const doc = await workspace.openTextDocument(Uri.parse(path))
+    await window.showTextDocument(doc, 1, false)
+    if (window.activeTextEditor) {
+        window.activeTextEditor.selection = new Selection(new Position(line - 1, position ? position : 0), new Position(line - 1, position ? position : 0))
+    } else {
+        await commands.executeCommand('cursorMove', {
+            to: 'up', by:'wrappedLine', value: doc.lineCount
+        })
+        await commands.executeCommand('cursorMove', {
+            to: 'down', by: 'wrappedLine', value: line - 1
+        })
+        window.activeTextEditor!.selection = new Selection(new Position(line - 1, position ? position : 0), new Position(line - 1, position ? position : 0))
+    }
 }
