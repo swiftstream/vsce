0ae01bd3cc0b267cf06ee4c872ed4897e1caca46
Mikhail Isaev <isaev.mihael@gmail.com>
2025-02-10 20:18:46 +0400

    🐳 Improve `web-prod` image

--- docker-images/web-prod/cmd.sh
+++ docker-images/web-prod/cmd.sh
@@ -13,3 +13,10 @@ escaped_crawlers=$(echo "$S_NGINX_CRAWLERS" | sed 's/\./\\\\./g')
 sed -i "s@{{CRAWLERS}}@$escaped_crawlers@g" "/etc/nginx/sites-available/default"
 # apply the config
 /etc/init.d/nginx restart
+
+# MARK: CrawlServer
+# load NVM
+export NVM_DIR="/root/.nvm"
+source "$NVM_DIR/nvm.sh"
+# run (keeps container running)
+/root/.nvm/versions/node/v22.10.0/bin/crawlserver /usr/share/nginx/html/app.wasm -p 3080 -g
