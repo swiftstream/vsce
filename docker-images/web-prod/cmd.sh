5e6392a8458635f14fd484b4c1357a34507266b9
Mikhail Isaev <isaev.mihael@gmail.com>
2025-02-14 04:13:38 +0400

    🐳 Web container: improve, add nginx brotli module

--- docker-images/web-prod/cmd.sh
+++ docker-images/web-prod/cmd.sh
@@ -3,20 +3,19 @@
 # MARK: NGINX
 # prepare the config
 export DOLLAR="$"
-if grep -q "DOLLAR" "/etc/nginx/sites-available/default"; then
-    echo "$(envsubst < "/etc/nginx/sites-available/default")" > /etc/nginx/sites-available/default
+if grep -q "DOLLAR" "$NGINX_CONFIG"; then
+    echo "$(envsubst < "$NGINX_CONFIG")" > "$NGINX_CONFIG"
 fi
 # Read the environment variable for crawlers list
 # Escape dots and double escape backslashes
 escaped_crawlers=$(echo "$S_NGINX_CRAWLERS" | sed 's/\./\\\\./g')
 # Replace the line with crawlers placeholder
-sed -i "s@{{CRAWLERS}}@$escaped_crawlers@g" "/etc/nginx/sites-available/default"
+sed -i "s@{{CRAWLERS}}@$escaped_crawlers@g" "$NGINX_CONFIG"
 # apply the config
 /etc/init.d/nginx restart
 
 # MARK: CrawlServer
 # load NVM
-export NVM_DIR="/root/.nvm"
 source "$NVM_DIR/nvm.sh"
 # run (keeps container running)
-/root/.nvm/versions/node/v22.10.0/bin/crawlserver /usr/share/nginx/html/app.wasm -p 3080 -g
+$NVM_DIR/versions/node/v$NODE_VERSION/bin/crawlserver /usr/share/nginx/html/app.wasm -p $CS_SERVER_PORT -g
