7ed37329e6ad6ba3d4fdb6ce0f569bb5275868af
Mikhail Isaev <isaev.mihael@gmail.com>
2025-02-15 08:16:26 +0400

    🐳 Web-dev: inherit from the production image

--- docker-images/web-dev/cmd.sh
+++ docker-images/web-dev/cmd.sh
@@ -155,13 +155,13 @@ fi
 # MARK: NGINX
 # prepare the config
 export DOLLAR="$"
-if grep -q "DOLLAR" "/etc/nginx/sites-available/default"; then
-    echo "$(envsubst < "/etc/nginx/sites-available/default")" > /etc/nginx/sites-available/default
+if grep -q "DOLLAR" "/etc/nginx/nginx.conf"; then
+    echo "$(envsubst < "/etc/nginx/nginx.conf")" > /etc/nginx/nginx.conf
 fi
 # Read the environment variable for crawlers list
 # Escape dots and double escape backslashes
 escaped_crawlers=$(echo "$S_NGINX_CRAWLERS" | sed 's/\./\\\\./g')
 # Replace the line with crawlers placeholder
-sed -i "s@{{CRAWLERS}}@$escaped_crawlers@g" "/etc/nginx/sites-available/default"
+sed -i "s@{{CRAWLERS}}@$escaped_crawlers@g" "/etc/nginx/nginx.conf"
 # apply the config
 /etc/init.d/nginx restart
