5d45d4ed98b2aaa583260ad2664e3aebdb741a05
Mikhail Isaev <isaev.mihael@gmail.com>
2025-03-12 04:16:30 +0400

    🔧 Improve `pgrep`

--- src/pgrep.ts
+++ src/pgrep.ts
@@ -12,20 +12,25 @@ export class Pgrep {
         if (!this.binPath)
             this.binPath = await this.stream.bash.which('pgrep')
         if (!this.binPath)
-            throw 'Path to gzip is undefined'
+            throw 'Path to pgrep is undefined'
         print(`executing pgrep ${args.join(' ')}`, LogLevel.Verbose)
         const result = await this.stream.bash.execute({
-            path: this.binPath!,
+            path: this.binPath,
             description: `pgrep`,
             cwd: cwd,
-            isCancelled: () => false
+            avoidPrintingError: true
         }, args)
         return result
     }
 
     async isAnyBlockingSwiftProcessRunning(): Promise<boolean> {
         if (!projectDirectory) return false
-        const result = await this.execute(['-fl', 'swift-(build|package)'], projectDirectory)
-        return result.stdout.includes('swift')
+        try {
+            const result = await this.execute(['-fl', `'swift-(build|package)'`], projectDirectory)
+            if (result.code != 0) return false
+            return result.stdout.includes('swift')
+        } catch {
+            return false
+        }
     }
 }
