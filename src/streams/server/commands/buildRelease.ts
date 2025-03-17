3dd43009cb2ad37192d3bfc3ad5d1c27eb46f2b8
Mikhail Isaev <isaev.mihael@gmail.com>
2025-03-18 03:02:13 +0400

    🪚 ServerStream: add `buildRelease` empty method

---
+++ src/streams/server/commands/buildRelease.ts
@@ -0,0 +1,6 @@
+import { LogLevel, print } from '../../stream'
+import { ServerStream } from '../serverStream'
+
+export async function buildRelease(serverStream: ServerStream, successCallback?: any) {
+    print('stream.buildRelease not implemented', LogLevel.Detailed)
+}
