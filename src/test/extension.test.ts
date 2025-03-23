1c4deac8f79844ad9bf141b7c8084fd5d0f1b211
Mikhail Isaev <isaev.mihael@gmail.com>
2025-03-23 05:05:31 +0400

    🧪 Improve tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,34 @@
-import * as assert from 'assert';
+import * as assert from 'assert'
+import * as vscode from 'vscode'
+import { DevContainerConfig } from '../devContainerConfig'
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+suite('Extension Test Suite', async () => {
+	vscode.window.showInformationMessage('Start all tests.')
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+	const extensionId = "swiftstream.swiftstream"
+
+	// Activate the extension
+	const extension = vscode.extensions.getExtension(extensionId)
+	assert.ok(extension, "Extension is not found")
+
+	console.log(`1 extension.isActive: ${extension.isActive}`)
+	
+	if (!extension.isActive) {
+		extension.activate()
+	}
+
+	while (!extension.isActive) {}
+
+	console.log(`2 extension.isActive: ${extension.isActive}`)
+
+	test('Dev Container Config test', async () => {
+		assert.ok(extension.isActive, "Extension did not activate")
+		let config = new DevContainerConfig()
+		assert.strictEqual(config.hasFeature('swiftstream/vsce:latest'), false)
+	})
 
 	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
-});
+		assert.strictEqual(-1, [1, 2, 3].indexOf(5))
+		assert.strictEqual(-1, [1, 2, 3].indexOf(0))
+	})
+})
