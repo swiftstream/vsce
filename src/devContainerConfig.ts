f54c4450dc6377ead72721cfd07279ee8e2fdcbe
Mikhail Isaev <isaev.mihael@gmail.com>
2025-04-29 23:30:40 +0400

    🪚 DevContainerConfig: implement generator methods

--- src/devContainerConfig.ts
+++ src/devContainerConfig.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 import JSON5 from 'json5'
-import { projectDirectory } from './extension'
+import { ExtensionStream, projectDirectory } from './extension'
+import { findTheRightToolchain } from './toolchain'
 
 export class DevContainerConfig {
     public static transaction(process: (config: DevContainerConfig) => void) {
@@ -180,4 +181,188 @@ export class DevContainerConfig {
         }
         return true
     }
+}
+
+// MARK: Generator
+
+export enum EmbeddedBranch {
+	Raspberry = 'Raspberry',
+	ESP32 = 'ESP32',
+	STM32 = 'STM32',
+	Zephyr = 'Zephyr'
+}
+
+export const generateAndWriteDevcontainerJson = (
+	pathTo: string,
+    stream: ExtensionStream,
+	swiftVersion: { major: number, minor: number },
+	options?: {
+		embedded?: {
+			branch: EmbeddedBranch
+		}
+	}
+): boolean => {
+    const generatedJson = generateDevcontainerJson(stream, swiftVersion, options)
+    if (!generatedJson) return false
+    const devContainerContent = JSON.stringify(generatedJson, null, '\t')
+    fs.writeFileSync(pathTo, devContainerContent, 'utf8')
+    return true
+}
+
+export const generateDevcontainerJson = (
+	stream: ExtensionStream,
+	swiftVersion: { major: number, minor: number },
+	options?: {
+		embedded?: {
+			branch: EmbeddedBranch
+		}
+	}
+): any | undefined => {
+	const toolchain = findTheRightToolchain(stream, { major: swiftVersion.major, minor: swiftVersion.minor })
+	let devcontainerObject = {
+		name: 'swiftstream',
+		build: {
+			dockerfile: 'Dockerfile',
+			args: {}
+		},
+		containerEnv: {
+			S_MODE: `${stream == ExtensionStream.Unknown ? ExtensionStream.Pure : stream}`,
+			S_TOOLCHAIN_URL_X86: toolchain.toolchain_urls.x86_64,
+			S_TOOLCHAIN_URL_ARM: toolchain.toolchain_urls.aarch64,
+			S_VERSION_MAJOR: `${toolchain.version.major}`,
+			S_VERSION_MINOR: `${toolchain.version.minor}`,
+			S_VERSION_PATCH: `${toolchain.version.patch}`
+		},
+		appPort: new Array<string>(),
+		otherPortsAttributes: {},
+		postStartCommand: 'cmd.sh',
+		customizations: {
+			vscode: {
+				extensions: [
+					'swiftstream.swiftstream',
+					'swiftlang.swift-vscode',
+					'mateocerquetella.xcode-12-theme'
+				],
+				settings: {
+					'extensions.ignoreRecommendations': true,
+					'remote.autoForwardPorts': false,
+					'swift.path': `/swift/toolchains/${toolchain.name}/usr/bin`,
+					'swift.swiftEnvironmentVariables': {
+						DEVELOPER_DIR: 'public'
+					},
+					'lldb.library': `/swift/toolchains/${toolchain.name}/usr/lib/liblldb.so`,
+					'lldb.launch.expressions': "native",
+					'swift.disableAutoResolve': false,
+					'swift.autoGenerateLaunchConfigurations': true,
+					'swift.backgroundCompilation': false,
+					'swift.showCreateSwiftProjectInWelcomePage': false,
+					'editor.semanticHighlighting.enabled': true,
+					'editor.fontFamily': 'Verdana, Verdana, Menlo, Monaco, \'Courier New\', monospace',
+					'editor.codeLensFontFamily': 'Verdana, Verdana, Menlo, Monaco, \'Courier New\', monospace'
+				}
+			}
+		},
+		capAdd: ['SYS_PTRACE'],
+		mounts: [
+			{ source: 'swift-toolchains', target: '/swift/toolchains', type: 'volume' },
+			{ source: 'swift-sdks', target: '/swift/sdks', type: 'volume' }
+		]
+	}
+	switch (stream) {
+		case ExtensionStream.Android:
+            if (toolchain.artifact_url) {
+                
+				devcontainerObject.containerEnv['S_ARTIFACT_ANDROID_URL'] = toolchain.artifact_url
+                devcontainerObject.containerEnv['S_ANDROID_VERSION'] = '24-0.1'
+			}
+			break
+		case ExtensionStream.Embedded:
+			if (options?.embedded) {
+				devcontainerObject.containerEnv['S_EMBEDDED_BRANCH'] = options.embedded.branch.toUpperCase()
+				switch (options.embedded.branch) {
+					case EmbeddedBranch.ESP32:
+						devcontainerObject.containerEnv['IDF_VERSION'] = '5.3'
+						devcontainerObject.containerEnv['IDF_CHIP_TARGETS'] = 'esp32c6'
+						devcontainerObject.containerEnv['IDF_TARGET'] = 'esp32c6'
+						devcontainerObject.containerEnv['IDF_PATH'] = '/embedded/esp'
+						devcontainerObject.containerEnv['IDF_TOOLS_PATH'] = '/embedded/esp/.espressif'
+						devcontainerObject.mounts.push({ source: 'esp-idf', target: '/embedded/esp', type: 'volume' })
+						break
+					case EmbeddedBranch.Raspberry:
+						devcontainerObject.containerEnv['PICO_SDK_VERSION'] = '2.1.1'
+						devcontainerObject.containerEnv['PICO_TOOLCHAIN_PATH'] = '/usr'
+						devcontainerObject.mounts.push({ source: 'raspberry', target: '/embedded/raspberry', type: 'volume' })
+						break
+					case EmbeddedBranch.STM32:
+						break
+					case EmbeddedBranch.Zephyr:
+						devcontainerObject.containerEnv['TOOLCHAINS'] = 'swift'
+						devcontainerObject.containerEnv['ZEPHYR_BASE'] = '/workspaces/zephyr'
+						devcontainerObject.mounts.push({ source: 'zephyr', target: '/workspaces/zephyr', type: 'volume' })
+						devcontainerObject.mounts.push({ source: 'zephyr-modules', target: '/workspaces/modules', type: 'volume' })
+						break
+				}
+			}
+			devcontainerObject.customizations.vscode.extensions.push('wokwi.wokwi-vscode')
+			break
+		case ExtensionStream.Server:
+			if (toolchain.artifact_url) {
+				devcontainerObject.containerEnv['S_ARTIFACT_STATIC_LINUX_URL'] = toolchain.artifact_url
+			}
+			break
+		case ExtensionStream.Web:
+            const crawlBots: string[] = [
+                'ahrefsbot',
+                'applebot',
+                'baiduspider',
+                'bingbot',
+                'developers.google.com',
+                'discordbot',
+                'duckduckbot',
+                'exabot',
+                'facebookexternalhit',
+                'gigabot',
+                'googlebot',
+                'ia_archiver',
+                'linkedinbot',
+                'mj12bot',
+                'pinterestbot',
+                'rogerbot',
+                'semrushbot',
+                'seznambot',
+                'skypeuripreview',
+                'slackbot',
+                'slurp',
+                'sogou',
+                'telegrambot',
+                'twitterbot',
+                'whatsapp',
+                'yahoo',
+                'yandex',
+                'yeti',
+                'yodaobot'
+            ]
+            devcontainerObject.containerEnv['S_NGINX_CRAWLERS'] = crawlBots.join('|')
+			devcontainerObject.appPort.push('7700:443')
+			devcontainerObject.appPort.push('8800:444')
+			devcontainerObject.appPort.push('9900:3080')
+			devcontainerObject.otherPortsAttributes['onAutoForward'] = 'ignore'
+			devcontainerObject.customizations.vscode.extensions.push('ms-vscode.wasm-dwarf-debugging')
+			if (toolchain.version.major >= 6) {
+				if (toolchain.artifact_urls?.wasi) {
+					devcontainerObject.containerEnv['S_ARTIFACT_WASI_URL'] = toolchain.artifact_urls.wasi
+				}
+				if (toolchain.artifact_urls?.wasip1_threads) {
+					devcontainerObject.containerEnv['S_ARTIFACT_WASIP1_THREADS_URL'] = toolchain.artifact_urls.wasip1_threads
+				}
+			}
+			break
+		default:
+			if (toolchain.artifact_url) {
+				devcontainerObject.containerEnv['S_ARTIFACT_STATIC_LINUX_URL'] = toolchain.artifact_url
+			}
+			break
+	}
+	
+	return devcontainerObject
 }
